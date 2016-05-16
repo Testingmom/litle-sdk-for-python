ed962a9f35a96527991e382627403ec99c44abdc
Ryan Vennell <ryan.vennell@gmail.com>
2016-05-16 16:41:40 -0500

    Updating pyxb version to latest.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='LitleSdkPython',
       url='http://www.litle.com/developers',
       packages=['litleSdkPython'],
       install_requires=[
-                        'PyXB==1.1.5',
+                        'PyXB==1.2.4',
                         'paramiko==1.14.0'],
       classifiers=[
           'Development Status :: 5 - Production/Stable',
